3661be5c38b85ec36995724fecedc533c88f5c00
Atul kumar <157561390+Atul-bhaiya@users.noreply.github.com>
2025-07-17 00:44:16 +0530

    Add files via upload

---
+++ Day6.py
@@ -0,0 +1,18 @@
+class Person:
+    def walk(self):
+        print("a man i walking ")
+    def talk(self):
+        print("a main is talking")
+
+class Student:
+    def study(self):
+        print("a man i walking ")
+        print()
+
+
+
+
+
+
+
+
