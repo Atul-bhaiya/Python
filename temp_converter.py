af1aa40d266c85ce4316ebaf94621102b13dfa2e
Atul kumar <157561390+Atul-bhaiya@users.noreply.github.com>
2025-07-22 01:32:59 +0530

    Add files via upload

---
+++ temp_converter.py
@@ -0,0 +1,9 @@
+#packages and Modules in Python  🧾 Mini Task – Temperature Converter (using module import)
+
+# temp_converter.py
+
+def celsius_to_fahrenheit(c):
+    return (c * 9/5) + 32
+
+def fahrenheit_to_celsius(f):
+    return (f - 32) * 5/9
